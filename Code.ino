02d0c555e586006ec23c3bac4ba573e778512be6
pingmyheart <pingmyheart@users.noreply.github.com>
2020-01-18 18:55:48 +0100

    Update Code.ino

--- Code.ino
+++ Code.ino
@@ -17,7 +17,7 @@ String minutestamp;
 ///////////////////////////////////////////////////
 
 //SEZIONE DI DEFINIZIONE PIN
-int relaycampana = 1;
+int bellRelay = 1;
 
 ////////////////////////////////////////////////////
 
@@ -25,69 +25,48 @@ EthernetUDP udp;
 unsigned int offset = 3600;
 NTPClient timeClient(udp, "europe.pool.ntp.org", offset);
 
-void setup()
-{
-  while(Ethernet.begin(mac)==false){
+String ringMinute = "15";
+
+void setup(){
+	while(Ethernet.begin(mac)==false){
       //keep trying to connect
-  }
+	}
   udp.begin(localport);//inizializza l'udp attraverso la porta scelta
   timeClient.begin();//inizializza il client per la ricezione di segnale
 
   //SEZIONE DI DEFINIZIONE PINMODE
-  pinMode(relaycampana, OUTPUT);
+  pinMode(bellRelay, OUTPUT);
 }
 
-void loop()
-{
-  while (!timeClient.update())
-  {
-    timeClient.forceUpdate();
-  }
+void loop(){
+	while (!timeClient.update()){
+		timeClient.forceUpdate();
+	}
 
 
-  formatteddate = timeClient.getFormattedTime();
+	formatteddate = timeClient.getFormattedTime();
 
-  int splitT = formatteddate.indexOf("T");
+	int splitT = formatteddate.indexOf("T");
 
-  daystamp = formatteddate.substring(0, splitT);
-  timestamp = formatteddate.substring(splitT + 1, formatteddate.length() - 1);
-  minutestamp = formatteddate.substring(splitT + 4, splitT + 5);
+	daystamp = formatteddate.substring(0, splitT);
+	timestamp = formatteddate.substring(splitT + 1, formatteddate.length() - 1);
+	minutestamp = formatteddate.substring(splitT + 4, splitT + 5);
     //CODICE PER FAR SUONARE UNA VOLTA LA CAMPANELLA AL MINUTO INDICATO
-    if (operative)
-    {
-      if (minutestamp == "15")
-      {
-        suona();
-        operative = false;
-      }
-    }
-    else 
-    {
-      if (minutestamp != "15")
-      {
-        operative = true;
-      }
-    }
-  else
-  {
-    digitalWrite(ledverde, LOW);
-    digitalWrite(ledrosso, HIGH);
-  }
-  delay(1000);
+	if (operative){
+		if (minutestamp == ringMinute){
+			suona();
+			operative = false;
+		}
+	}
+	else if(minutestamp != ringMinute){
+		operative = true;
+	}
+	delay(1000);
 }
 
 void suona()
 {
-  digitalWrite(relaycampana, HIGH);
-  delay(5000);
-  digitalWrite(relaycampana, LOW);
+	digitalWrite(bellRelay, HIGH);
+	delay(5000);
+	digitalWrite(bellRelay, LOW);
 }
-/*
-MATERIALE UTILE:
-Arduino Uno
-Shield Ethernet
-Dispay 16X2 con modulo I2C
-Cavo Ethernet
-Alimentatore Arduino
-Jumper
-*/
